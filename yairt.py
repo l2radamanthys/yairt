15c14e7be87e3125e13d5b508ac925ae7ffb443c
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2018-08-13 21:52:52 -0300

    .

--- yairt.py
+++ yairt.py
@@ -3,14 +3,32 @@ import os
 import uuid
 from sys import argv
 
+# Flags
+dir__ = False 
+wx__ = False
+hx__ = False
+
 
 def main():
     size = 128, 128
+    _argv = argv[1:] #first element is script name
+    dir__ = argv.index('-d')
+    if dir__ == -1:
+        _argv.pop(dir__)
+        dir__ = True
+
+    if len(map())
+
+    #
+    # if dir__ == -1:
+    #    _argv.pop(dir__)
+    #    dir__ = True
 
     path = None
-    if len(argv) >= 2:
-        path = argv[1]
+    if len(argv) >= 1:
+        path = argv[0]
     
+
     filename, ext = os.path.splitext(path)
     uid = uuid.uuid4()
     img = Image.open(path)
